2ff5a156d889b832507aa494217f4d0220c821ef
mickey <mickey.shaul@gmail.com>
2020-03-02 16:23:14 +0200

    update shell scripts

--- webinar-step3.sh
+++ webinar-step3.sh
@@ -1,8 +1,8 @@
 ## Create a clone from the Snapshot and connect a new POD
 echo "Create clone and connect a new POD "
 echo "================================== "
-kubectl create -f step2-snapshot/pvc-from-snap.yaml
-kubectl create -f step2-snapshot/pod-snap.yaml
+kubectl create -f step3-clone/pvc-from-snap.yaml
+kubectl create -f step3-clone/pod-snap.yaml
 
 ## Execute into the POD and list the files
 sleep 20
